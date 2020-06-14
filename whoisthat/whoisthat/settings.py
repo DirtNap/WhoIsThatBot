5ece788dad037752685401ce3b6473d31041e8c8
Michael Donnelly <MichaelVDonnelly@gmail.com>
2020-06-14 19:48:18 -0400

    Use django-environ for per-environment settings configuration.

--- whoisthat/whoisthat/settings.py
+++ whoisthat/whoisthat/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import environ
+
+env = environ.Env(DEBUG=(bool, False))
+
+environ.Env.read_env()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,10 +26,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&fi^vv@17ppkv7p4q@%05*(4-8^_*vw+c8nbyg8g+ylum7_w$n'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -120,3 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# PRAW settings
+
+PRAW_CLIENT_ID = env('PRAW_CLIENT_ID')
+PRAW_CLIENT_SECRET = env('PRAW_CLIENT_SECRET')
+PRAW_CLIENT_USERNAME = env('PRAW_CLIENT_USERNAME')
+PRAW_CLIENT_PASSWORD = env('PRAW_CLIENT_PASSWORD')
